9f320e53c83c82532bc7486e47052969a21245a2
Thelordgreat <greatokafor15@gmail.com>
2022-06-12 14:26:29 +0100

    minor correction

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import SignIn from './Components/LoginPage/SignIn';
 import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
 import Signup from './Components/SignUp/Signup.js';
-//import Nav from './Components/Nav/Nav';
+import Nav from './Components/Nav/Nav';
 
 
 
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
       <Router>
+        <Nav/>
         <Routes>
           <Route path='/' element={<SignIn/>}/>
           <Route path='/signup' element={<Signup/>}/>
